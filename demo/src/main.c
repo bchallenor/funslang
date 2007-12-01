8ff632f3095c41e016fdfd5d55cb5f13850161f1
ben <ben@challenor.org>
2007-12-01 13:36:25 +0000

    more complicated
    [git-p4: depot-paths = "//depot/project/": change = 60]

--- demo/src/main.c
+++ demo/src/main.c
@@ -14,10 +14,12 @@ GLuint g_FragProgramId = 0;
 
 const char g_FragProgramCode[] =
 "!!NVfp4.0\n"
-"FLOAT PARAM color = program.local[0];\n"
-"MOV result.color, color;\n"
+"FLOAT PARAM color[3] = {program.local[0..2]};\n"
+"INT PARAM idx = program.local[3];\n"
+//"INT TEMP idx2;\n"
+//"MOV.S idx2, idx;\n"
+"MOV.F result.color, color[idx.x];\n"
 "END";
-
 
 void render(void)
 {
@@ -51,7 +53,10 @@ int main(int argc, char** argv)
 	assert(g_FragProgramId);
 
 	// Enable shader.
-	glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 0, 0.0, 1.0, 1.0, 0.0);
+	glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 0, 1.0, 0.0, 0.0, 0.0);
+	glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 1, 0.0, 1.0, 0.0, 0.0);
+	glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 2, 0.0, 0.0, 1.0, 0.0);
+	glProgramLocalParameterI4iNV(GL_FRAGMENT_PROGRAM_ARB, 3, 1, 0, 0, 0);
 	glEnable(GL_FRAGMENT_PROGRAM_ARB);
 
 	// Enter main loop.
