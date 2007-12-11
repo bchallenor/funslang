24b61a11ebf7e5c556d2abb15966bb93ba27dd8d
ben <ben@challenor.org>
2007-12-11 12:27:56 +0000

    some thoughts
    [git-p4: depot-paths = "//depot/project/": change = 111]

--- haskell/Library.hs
+++ haskell/Library.hs
@@ -10,27 +10,25 @@ data Operator1
 
 
 data Operator2
-  = Op2Subscript
-  | Op2Swizzle
-  | Op2Append
-  | Op2Mul
+  = Op2Subscript -- t n -> (Int -> t)
+  | Op2Swizzle -- t n -> Int m -> (t m)
+  | Op2Append -- t n -> t m -> (t m+n)
+  | Op2Mul -- i/f -> i/f -> (i/f); i/f n -> i/f n -> (i/f n); i/f m n -> i/f m n -> (i/f m n)
   | Op2Div
-  | Op2LinearMul
-  | Op2ScaleMul
+  | Op2LinearMul -- i/f q p -> i/f r q -> i/f r p; i/f q -> i/f r q -> i/f r; i/f q p -> i/f q -> i/f p
+  | Op2ScaleMul -- i/f n -> (i/f -> i/f n)
   | Op2ScaleDiv
   | Op2Add
   | Op2Sub
-  | Op2LessThan
+  | Op2LessThan -- i/f -> i/f -> b
   | Op2LessThanEqual
   | Op2GreaterThan
   | Op2GreaterThanEqual
-  | Op2Equal
+  | Op2Equal -- t -> t -> b (t not function)
   | Op2NotEqual
   | Op2Identical
   | Op2NotIdentical
-  | Op2And
-  | Op2Or
+  | Op2And -- b -> b -> b
+  | Op2Or -- b -> b -> b
   
   deriving (Show, Eq)
-
-
