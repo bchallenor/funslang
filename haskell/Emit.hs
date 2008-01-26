0d01bad1373dfb22e11c86c39a9a0804748354d0
ben <ben@challenor.org>
2008-01-26 19:24:29 +0000

    renamed loc -> name
    [git-p4: depot-paths = "//depot/project/": change = 243]

--- haskell/Emit.hs
+++ haskell/Emit.hs
@@ -27,7 +27,7 @@ emit sk si (g, mnv, mvn) =
       map (\v -> let Just n = Map.lookup v mvn in emitNode (sk, si, mnv) n) vs
 
 
-
+-- The names that GLSL gives to Funslang varyings.
 emitVaryingQualifier :: ShaderKind -> String
 emitVaryingQualifier ShaderKindVertex = "attribute"
 emitVaryingQualifier ShaderKindFragment = "varying"
@@ -44,44 +44,44 @@ emitPackingType n = assert (n > 0 && n <= maxPackingSize) "vec" ++ show n
 
 -- These emit variable names.
 
-emitRootLocUniform :: ShaderKind -> String
-emitRootLocUniform ShaderKindVertex = "VertexUniforms"
-emitRootLocUniform ShaderKindFragment = "FragmentUniforms"
+emitRootNameUniform :: ShaderKind -> String
+emitRootNameUniform ShaderKindVertex = "VertexUniforms"
+emitRootNameUniform ShaderKindFragment = "FragmentUniforms"
 
-emitLocUniform :: ShaderKind -> Int -> String
-emitLocUniform sk i = emitRootLocUniform sk ++ "[" ++ show i ++ "]"
+emitNameUniform :: ShaderKind -> Int -> String
+emitNameUniform sk i = emitRootNameUniform sk ++ "[" ++ show i ++ "]"
 
-emitRootLocVarying :: ShaderKind -> String
-emitRootLocVarying ShaderKindVertex = "VertexVaryings"
-emitRootLocVarying ShaderKindFragment = "FragmentVaryings"
+emitRootNameVarying :: ShaderKind -> String
+emitRootNameVarying ShaderKindVertex = "VertexVaryings"
+emitRootNameVarying ShaderKindFragment = "FragmentVaryings"
 
-emitLocVarying :: ShaderKind -> ShaderInputs -> Int -> String
-emitLocVarying sk si i =
+emitNameVarying :: ShaderKind -> ShaderInputs -> Int -> String
+emitNameVarying sk si i =
   let (d, m) = i `divMod` maxPackingSize in
-    emitRootLocVarying sk ++ show (maxPackingSize * d) ++
+    emitRootNameVarying sk ++ show (maxPackingSize * d) ++
       if i == (num_varyings si - 1) && m == 0
         then "" -- no need to subscript: it's the last element, and that element is packed into a float
         else "[" ++ show m ++ "]"
 
-emitLocTexture :: Int -> String
-emitLocTexture i = "Tex" ++ show i
+emitNameTexture :: Int -> String
+emitNameTexture i = "Tex" ++ show i
 
-emitLocDFVertex :: Vertex -> String
-emitLocDFVertex v = "t" ++ show v
+emitNameDFVertex :: Vertex -> String
+emitNameDFVertex v = "t" ++ show v
 
-emitLocDF :: Map.Map DF Vertex -> DF -> String
-emitLocDF mnv n = case Map.lookup n mnv of
-  Just v -> emitLocDFVertex v
+emitNameDF :: Map.Map DF Vertex -> DF -> String
+emitNameDF mnv n = case Map.lookup n mnv of
+  Just v -> emitNameDFVertex v
   Nothing -> error "unknown vertex"
 
-emitLocDFReal :: Map.Map DF Vertex -> DFReal -> String
-emitLocDFReal mnv df = emitLocDF mnv $ DFReal df
+emitNameDFReal :: Map.Map DF Vertex -> DFReal -> String
+emitNameDFReal mnv df = emitNameDF mnv $ DFReal df
 
-emitLocDFBool :: Map.Map DF Vertex -> DFBool -> String
-emitLocDFBool mnv df = emitLocDF mnv $ DFBool df
+emitNameDFBool :: Map.Map DF Vertex -> DFBool -> String
+emitNameDFBool mnv df = emitNameDF mnv $ DFBool df
 
-emitLocDFSample :: Map.Map DF Vertex -> DFSample -> String
-emitLocDFSample mnv df = emitLocDF mnv $ DFSample df
+emitNameDFSample :: Map.Map DF Vertex -> DFSample -> String
+emitNameDFSample mnv df = emitNameDF mnv $ DFSample df
 
 
 -- Emits a function call (using emitted strings).
@@ -95,20 +95,20 @@ emitStrAssign d a = d ++ " = " ++ a ++ ";"
 
 -- Emits a unary prefix operator, assigning its result.
 emitUnOpAssign :: Map.Map DF Vertex -> DF -> String -> DF -> String
-emitUnOpAssign mnv d op r = emitStrAssign (emitLocDF mnv d) (op ++ " " ++ emitLocDF mnv r)
+emitUnOpAssign mnv d op r = emitStrAssign (emitNameDF mnv d) (op ++ " " ++ emitNameDF mnv r)
 
 -- Emits a binary infix operator, assigning its result.
 emitBinOpAssign :: Map.Map DF Vertex -> DF -> DF -> String -> DF -> String
-emitBinOpAssign mnv d l op r = emitStrAssign (emitLocDF mnv d) (emitLocDF mnv l ++ " " ++ op ++ " " ++ emitLocDF mnv r)
+emitBinOpAssign mnv d l op r = emitStrAssign (emitNameDF mnv d) (emitNameDF mnv l ++ " " ++ op ++ " " ++ emitNameDF mnv r)
 
 -- Emits a function call, assigning its result.
 emitFunAssign :: Map.Map DF Vertex -> DF -> String -> [DF] -> String
-emitFunAssign mnv d f args = emitStrAssign (emitLocDF mnv d) (emitStrFun f $ map (emitLocDF mnv) args)
+emitFunAssign mnv d f args = emitStrAssign (emitNameDF mnv d) (emitStrFun f $ map (emitNameDF mnv) args)
 
 
 -- Emits uniforms declaration.
 emitUniformsDecl :: ShaderKind -> ShaderInputs -> String
-emitUniformsDecl sk si = "uniform float " ++ emitLocUniform sk (num_uniforms si) ++ ";"
+emitUniformsDecl sk si = "uniform float " ++ emitNameUniform sk (num_uniforms si) ++ ";"
 
 -- Emits varyings declarations.
 emitVaryingsDecls :: ShaderKind -> ShaderInputs -> [String]
@@ -121,7 +121,7 @@ emitVaryingsDecls' sk si num_packed acc =
       then acc
       else
         let num_now = min num_left maxPackingSize in
-        let decl = emitVaryingQualifier sk ++ " " ++ emitPackingType num_now ++ " " ++ emitRootLocVarying sk ++ show num_packed ++ ";" in
+        let decl = emitVaryingQualifier sk ++ " " ++ emitPackingType num_now ++ " " ++ emitRootNameVarying sk ++ show num_packed ++ ";" in
           emitVaryingsDecls' sk si (num_packed + num_now) (decl : acc)
 
 -- Emits texture declarations.
@@ -129,10 +129,10 @@ emitTextureDecls :: ShaderInputs -> [String]
 emitTextureDecls si = map emitTextureDecl (textures si)
 
 emitTextureDecl :: ShaderTextureInput -> String
-emitTextureDecl (ShaderTextureInput1D i) = "uniform sampler1D " ++ emitLocTexture i ++ ";"
-emitTextureDecl (ShaderTextureInput2D i) = "uniform sampler2D " ++ emitLocTexture i ++ ";"
-emitTextureDecl (ShaderTextureInput3D i) = "uniform sampler3D " ++ emitLocTexture i ++ ";"
-emitTextureDecl (ShaderTextureInputCube i) = "uniform samplerCube " ++ emitLocTexture i ++ ";"
+emitTextureDecl (ShaderTextureInput1D i) = "uniform sampler1D " ++ emitNameTexture i ++ ";"
+emitTextureDecl (ShaderTextureInput2D i) = "uniform sampler2D " ++ emitNameTexture i ++ ";"
+emitTextureDecl (ShaderTextureInput3D i) = "uniform sampler3D " ++ emitNameTexture i ++ ";"
+emitTextureDecl (ShaderTextureInputCube i) = "uniform samplerCube " ++ emitNameTexture i ++ ";"
 
 -- Emits all relevant declarations.
 emitDecls :: ShaderKind -> ShaderInputs -> [String]
@@ -142,26 +142,26 @@ emitDecls sk si = emitUniformsDecl sk si : emitTextureDecls si ++ emitVaryingsDe
 -- Emits the operation represented by a DF.
 emitNode :: (ShaderKind, ShaderInputs, Map.Map DF Vertex) -> DF -> String
 
-emitNode (_, _, mnv) n@(DFReal (DFRealLiteral d)) = emitStrAssign (emitLocDF mnv n) $ show d
-emitNode (sk, si, mnv) n@(DFReal (DFRealVarying i)) = emitStrAssign (emitLocDF mnv n) $ emitLocVarying sk si i
-emitNode (sk, _, mnv) n@(DFReal (DFRealUniform i)) = emitStrAssign (emitLocDF mnv n) $ emitLocUniform sk i
+emitNode (_, _, mnv) n@(DFReal (DFRealLiteral d)) = emitStrAssign (emitNameDF mnv n) $ show d
+emitNode (sk, si, mnv) n@(DFReal (DFRealVarying i)) = emitStrAssign (emitNameDF mnv n) $ emitNameVarying sk si i
+emitNode (sk, _, mnv) n@(DFReal (DFRealUniform i)) = emitStrAssign (emitNameDF mnv n) $ emitNameUniform sk i
 
-emitNode (_, _, mnv) n@(DFReal (DFRealCond cond p q)) = emitStrAssign (emitLocDF mnv n) $ (emitLocDFBool mnv cond) ++ " ? " ++ (emitLocDFReal mnv p) ++ " : " ++ (emitLocDFReal mnv q)
+emitNode (_, _, mnv) n@(DFReal (DFRealCond cond p q)) = emitStrAssign (emitNameDF mnv n) $ (emitNameDFBool mnv cond) ++ " ? " ++ (emitNameDFReal mnv p) ++ " : " ++ (emitNameDFReal mnv q)
 
 emitNode (_, _, mnv) n@(DFReal (DFRealAdd p q)) = emitBinOpAssign mnv n (DFReal p) "+" (DFReal q)
 emitNode (_, _, mnv) n@(DFReal (DFRealSub p q)) = emitBinOpAssign mnv n (DFReal p) "-" (DFReal q)
 emitNode (_, _, mnv) n@(DFReal (DFRealMul p q)) = emitBinOpAssign mnv n (DFReal p) "*" (DFReal q)
 emitNode (_, _, mnv) n@(DFReal (DFRealDiv p q)) = emitBinOpAssign mnv n (DFReal p) "/" (DFReal q)
 emitNode (_, _, mnv) n@(DFReal (DFRealNeg p)) = emitUnOpAssign mnv n "-" (DFReal p)
-emitNode (_, _, mnv) n@(DFReal (DFRealRcp p)) = emitStrAssign (emitLocDF mnv n) $ "1 / " ++ (emitLocDFReal mnv p)
+emitNode (_, _, mnv) n@(DFReal (DFRealRcp p)) = emitStrAssign (emitNameDF mnv n) $ "1 / " ++ (emitNameDFReal mnv p)
 emitNode (_, _, mnv) n@(DFReal (DFRealRsq p)) = emitFunAssign mnv n "inversesqrt" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealAbs p)) = emitFunAssign mnv n "abs" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealMin p q)) = emitFunAssign mnv n "min" [DFReal p, DFReal q]
 emitNode (_, _, mnv) n@(DFReal (DFRealMax p q)) = emitFunAssign mnv n "max" [DFReal p, DFReal q]
 emitNode (_, _, mnv) n@(DFReal (DFRealFloor p)) = emitFunAssign mnv n "floor" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealCeiling p)) = emitFunAssign mnv n "ceil" [DFReal p]
-emitNode (_, _, mnv) n@(DFReal (DFRealRound p)) = emitStrAssign (emitLocDF mnv n) ("float(int(" ++ emitLocDFReal mnv p ++ " + (" ++ emitLocDFReal mnv p ++ " < 0 ? -0.5 : 0.5)))")
-emitNode (_, _, mnv) n@(DFReal (DFRealTruncate p)) = emitStrAssign (emitLocDF mnv n) ("float(int(" ++ emitLocDFReal mnv p ++ "))")
+emitNode (_, _, mnv) n@(DFReal (DFRealRound p)) = emitStrAssign (emitNameDF mnv n) ("float(int(" ++ emitNameDFReal mnv p ++ " + (" ++ emitNameDFReal mnv p ++ " < 0 ? -0.5 : 0.5)))")
+emitNode (_, _, mnv) n@(DFReal (DFRealTruncate p)) = emitStrAssign (emitNameDF mnv n) ("float(int(" ++ emitNameDFReal mnv p ++ "))")
 emitNode (_, _, mnv) n@(DFReal (DFRealFract p)) = emitFunAssign mnv n "fract" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealExp p)) = emitFunAssign mnv n "exp" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealExp2 p)) = emitFunAssign mnv n "exp2" [DFReal p]
@@ -175,16 +175,16 @@ emitNode (_, _, mnv) n@(DFReal (DFRealASin p)) = emitFunAssign mnv n "asin" [DFR
 emitNode (_, _, mnv) n@(DFReal (DFRealACos p)) = emitFunAssign mnv n "acos" [DFReal p]
 emitNode (_, _, mnv) n@(DFReal (DFRealATan p)) = emitFunAssign mnv n "atan" [DFReal p]
 
-emitNode (_, _, mnv) n@(DFReal (DFRealGetTexR p)) = emitStrAssign (emitLocDF mnv n) (emitLocDFSample mnv p ++ ".r")
-emitNode (_, _, mnv) n@(DFReal (DFRealGetTexG p)) = emitStrAssign (emitLocDF mnv n) (emitLocDFSample mnv p ++ ".g")
-emitNode (_, _, mnv) n@(DFReal (DFRealGetTexB p)) = emitStrAssign (emitLocDF mnv n) (emitLocDFSample mnv p ++ ".b")
-emitNode (_, _, mnv) n@(DFReal (DFRealGetTexA p)) = emitStrAssign (emitLocDF mnv n) (emitLocDFSample mnv p ++ ".a")
+emitNode (_, _, mnv) n@(DFReal (DFRealGetTexR p)) = emitStrAssign (emitNameDF mnv n) (emitNameDFSample mnv p ++ ".r")
+emitNode (_, _, mnv) n@(DFReal (DFRealGetTexG p)) = emitStrAssign (emitNameDF mnv n) (emitNameDFSample mnv p ++ ".g")
+emitNode (_, _, mnv) n@(DFReal (DFRealGetTexB p)) = emitStrAssign (emitNameDF mnv n) (emitNameDFSample mnv p ++ ".b")
+emitNode (_, _, mnv) n@(DFReal (DFRealGetTexA p)) = emitStrAssign (emitNameDF mnv n) (emitNameDFSample mnv p ++ ".a")
 
-emitNode (_, _, mnv) n@(DFBool (DFBoolLiteral b)) = emitStrAssign (emitLocDF mnv n) $ show b
-emitNode (sk, si, mnv) n@(DFBool (DFBoolVarying i)) = emitStrAssign (emitLocDF mnv n) $ "bool(" ++ emitLocVarying sk si i ++ ")"
-emitNode (sk, _, mnv) n@(DFBool (DFBoolUniform i)) = emitStrAssign (emitLocDF mnv n) $ "bool(" ++ emitLocUniform sk i ++ ")"
+emitNode (_, _, mnv) n@(DFBool (DFBoolLiteral b)) = emitStrAssign (emitNameDF mnv n) $ show b
+emitNode (sk, si, mnv) n@(DFBool (DFBoolVarying i)) = emitStrAssign (emitNameDF mnv n) $ "bool(" ++ emitNameVarying sk si i ++ ")"
+emitNode (sk, _, mnv) n@(DFBool (DFBoolUniform i)) = emitStrAssign (emitNameDF mnv n) $ "bool(" ++ emitNameUniform sk i ++ ")"
 
-emitNode (_, _, mnv) n@(DFBool (DFBoolCond cond p q)) = emitStrAssign (emitLocDF mnv n) $ (emitLocDFBool mnv cond) ++ " ? " ++ (emitLocDFBool mnv p) ++ " : " ++ (emitLocDFBool mnv q)
+emitNode (_, _, mnv) n@(DFBool (DFBoolCond cond p q)) = emitStrAssign (emitNameDF mnv n) $ (emitNameDFBool mnv cond) ++ " ? " ++ (emitNameDFBool mnv p) ++ " : " ++ (emitNameDFBool mnv q)
 
 emitNode (_, _, mnv) n@(DFBool (DFBoolLessThan p q)) = emitBinOpAssign mnv n (DFReal p) "<" (DFReal q)
 emitNode (_, _, mnv) n@(DFBool (DFBoolLessThanEqual p q)) = emitBinOpAssign mnv n (DFReal p) "<=" (DFReal q)
@@ -200,7 +200,7 @@ emitNode (_, _, mnv) n@(DFBool (DFBoolAnd p q)) = emitBinOpAssign mnv n (DFBool 
 emitNode (_, _, mnv) n@(DFBool (DFBoolOr p q)) = emitBinOpAssign mnv n (DFBool p) "||" (DFBool q)
 emitNode (_, _, mnv) n@(DFBool (DFBoolNot p)) = emitUnOpAssign mnv n "!" (DFBool p)
 
-emitNode (_, _, mnv) n@(DFSample (DFSample1D i p)) = emitStrAssign (emitLocDF mnv n) (emitStrFun "texture1D" [emitLocTexture i, emitLocDFReal mnv p])
-emitNode (_, _, mnv) n@(DFSample (DFSample2D i p q)) = emitStrAssign (emitLocDF mnv n) (emitStrFun "texture2D" [emitLocTexture i, "vec2(" ++ emitLocDFReal mnv p ++ ", " ++ emitLocDFReal mnv q ++ ")"]) 
-emitNode (_, _, mnv) n@(DFSample (DFSample3D i p q r)) = emitStrAssign (emitLocDF mnv n) (emitStrFun "texture3D" [emitLocTexture i, "vec3(" ++ emitLocDFReal mnv p ++ ", " ++ emitLocDFReal mnv q ++ ", " ++ emitLocDFReal mnv r ++ ")"]) 
-emitNode (_, _, mnv) n@(DFSample (DFSampleCube i p q r)) = emitStrAssign (emitLocDF mnv n) (emitStrFun "textureCube" [emitLocTexture i, "vec3(" ++ emitLocDFReal mnv p ++ ", " ++ emitLocDFReal mnv q ++ ", " ++ emitLocDFReal mnv r ++ ")"]) 
+emitNode (_, _, mnv) n@(DFSample (DFSample1D i p)) = emitStrAssign (emitNameDF mnv n) (emitStrFun "texture1D" [emitNameTexture i, emitNameDFReal mnv p])
+emitNode (_, _, mnv) n@(DFSample (DFSample2D i p q)) = emitStrAssign (emitNameDF mnv n) (emitStrFun "texture2D" [emitNameTexture i, "vec2(" ++ emitNameDFReal mnv p ++ ", " ++ emitNameDFReal mnv q ++ ")"]) 
+emitNode (_, _, mnv) n@(DFSample (DFSample3D i p q r)) = emitStrAssign (emitNameDF mnv n) (emitStrFun "texture3D" [emitNameTexture i, "vec3(" ++ emitNameDFReal mnv p ++ ", " ++ emitNameDFReal mnv q ++ ", " ++ emitNameDFReal mnv r ++ ")"]) 
+emitNode (_, _, mnv) n@(DFSample (DFSampleCube i p q r)) = emitStrAssign (emitNameDF mnv n) (emitStrFun "textureCube" [emitNameTexture i, "vec3(" ++ emitNameDFReal mnv p ++ ", " ++ emitNameDFReal mnv q ++ ", " ++ emitNameDFReal mnv r ++ ")"]) 
