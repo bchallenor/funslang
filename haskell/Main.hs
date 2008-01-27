2b7416dc9db0554d7877eaa2b7b81296bb89de6c
ben <ben@challenor.org>
2008-01-27 18:36:39 +0000

    set up interface on the haskell side; now to figure out how to compile it all together
    [git-p4: depot-paths = "//depot/project/": change = 259]

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,8 +1,11 @@
+{-# OPTIONS -ffi #-}
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as ByteString
 import Control.Monad.Error
 import System.IO
+import Foreign
+import Foreign.C
 
 import Parser
 import System.Directory
@@ -15,8 +18,7 @@ import Interpreter
 import Dataflow
 import Emit
 
-test :: IO ()
-test = withArgs ["test"] main
+-- Functions shared between the standalone compiler and the C interface.
 
 compile :: ShaderKind -> ByteString.ByteString -> Either String (Expr, Type, Value, ShaderInputOutput, DFGraph)
 compile sk bs = do
@@ -25,6 +27,9 @@ compile sk bs = do
   (t, vrefs'') <- inferExprType gamma e vrefs'
   (v, si) <- interpretExprAsShader sk env e t
   return (e, t, v, si, dependencyGraph v)
+
+
+-- Functions for use as a standalone compiler.
 
 compileAndPrint :: ShaderKind -> ByteString.ByteString -> IO ()
 compileAndPrint sk bs = do
@@ -58,3 +63,45 @@ main = do
       bsfp <- ByteString.readFile $ a ++ ".fp"
       compileAndPrint ShaderKindFragment bsfp
     else return ()
+
+test :: IO ()
+test = withArgs ["test"] main
+
+
+-- The following functions export the compiler to C.
+
+fsCompileFunslang :: ShaderKind -> CString -> Ptr Bool -> Ptr CInt -> Ptr CInt -> Ptr CString -> IO ()
+fsCompileFunslang sk path_cstr success_ptr num_uniforms_ptr num_varyings_ptr emit_cstr_ptr = do
+  path <- peekCString path_cstr
+  exists <- doesFileExist path
+  if exists
+    then do
+      bs <- ByteString.readFile path
+      case compile sk bs of
+        Right (_, _, _, si, g) -> do
+          emit_cstr <- newCString $ emit sk si g
+          poke success_ptr True
+          poke num_uniforms_ptr $ fromIntegral $ num_uniforms si
+          poke num_varyings_ptr $ fromIntegral $ num_varyings si
+          poke emit_cstr_ptr emit_cstr
+        Left msg -> do
+          msg_cstr <- newCString msg
+          poke success_ptr False
+          poke emit_cstr_ptr msg_cstr
+    else do
+      msg_cstr <- newCString $ "file <" ++ path ++ "> does not exist"
+      poke success_ptr False
+      poke emit_cstr_ptr msg_cstr
+
+fsCompileFunslangVertex :: CString -> Ptr Bool -> Ptr CInt -> Ptr CInt -> Ptr CString -> IO ()
+fsCompileFunslangVertex = fsCompileFunslang ShaderKindVertex
+
+fsCompileFunslangFragment :: CString -> Ptr Bool -> Ptr CInt -> Ptr CInt -> Ptr CString -> IO ()
+fsCompileFunslangFragment = fsCompileFunslang ShaderKindFragment
+
+fsCompileFree :: CString -> IO ()
+fsCompileFree cstr = free cstr
+
+foreign export ccall "fsCompileFunslangVertex" fsCompileFunslangVertex :: CString -> Ptr Bool -> Ptr CInt -> Ptr CInt -> Ptr CString -> IO ()
+foreign export ccall "fsCompileFunslangFragment" fsCompileFunslangFragment :: CString -> Ptr Bool -> Ptr CInt -> Ptr CInt -> Ptr CString -> IO ()
+foreign export ccall "fsCompileFree" fsCompileFree :: CString -> IO ()
