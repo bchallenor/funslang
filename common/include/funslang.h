dbd83404f46091a381300a6a53c3f38595c6a387
ben <ben@challenor.org>
2008-02-12 02:26:03 +0000

    everything seems to work except make install
    [git-p4: depot-paths = "//depot/project/": change = 299]

--- common/include/funslang.h
+++ common/include/funslang.h
@@ -3,6 +3,13 @@
 
 #include <GL/glew.h>
 
+#ifdef FUNSLANG_BUILDING_DLL
+// don't need dllexport as we're using a .def file
+#define FUNSLANG_API
+#else
+#define FUNSLANG_API __declspec(dllimport)
+#endif
+
 typedef struct
 {
 	const char* vertex_shader_path;
@@ -22,26 +29,26 @@ typedef struct
 	GLint loc_fragment_uniforms;
 } FSprogram;
 
-void fsInit(int* argc, char*** argv);
-void fsExit(void);
+FUNSLANG_API void fsInit(int* argc, char*** argv);
+FUNSLANG_API void fsExit(void);
 
 // Funslang compilation.
 // Requires p->vertex_shader_path and p->fragment_shader_path to be set.
 // The rest of p can be uninitialized.
-bool fsCompile(FSprogram* p);
+FUNSLANG_API bool fsCompile(FSprogram* p);
 
 // Shader data initialization.
-void fsSetVertexUniforms(FSprogram* p, const GLfloat* data);
-void fsSetFragmentUniforms(FSprogram* p, const GLfloat* data);
-void fsSetVertexVaryings(FSprogram* p, const GLfloat* data);
+FUNSLANG_API void fsSetVertexUniforms(FSprogram* p, const GLfloat* data);
+FUNSLANG_API void fsSetFragmentUniforms(FSprogram* p, const GLfloat* data);
+FUNSLANG_API void fsSetVertexVaryings(FSprogram* p, const GLfloat* data);
 
 // Shader texture image unit assignment.
-void fsSetTextureImageUnits(FSprogram* p);
+FUNSLANG_API void fsSetTextureImageUnits(FSprogram* p);
 
 // Loads texture from file and assigns it new name (using glGenTextures).
 // Creates mipmap and enables trilinear filtering.
 // Currently only supports JPG.
 // Returns name, or 0 on error (note 0 is not a valid GL texture name).
-GLuint fsLoadTexture2D(const char* fn);
+FUNSLANG_API GLuint fsLoadTexture2D(const char* fn);
 
 #endif // FUNSLANG_H
