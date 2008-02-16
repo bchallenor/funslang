515309afe357abe5c877fa9054aa152a96a7e96a
ben <ben@challenor.org>
2008-02-16 17:16:19 +0000

    removing old vs proj
    [git-p4: depot-paths = "//depot/project/": change = 305]

--- demo/src/shaders.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHADERS_H
-#define SHADERS_H
-
-#include <GL/glew.h>
-
-// len is that returned by strlen, i.e. without terminator.
-GLuint compileARBShader(GLenum type, const char* code, const GLsizei len);
-
-#endif // SHADERS_H
