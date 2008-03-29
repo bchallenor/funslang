f94565fb47baf9adc293e65329bb0b6440e21443
ben <ben@challenor.org>
2008-03-29 21:38:28 +0000

    changing stereo textures
    [git-p4: depot-paths = "//depot/project/": change = 395]

--- demos/stereo/stereo.c
+++ demos/stereo/stereo.c
@@ -248,7 +248,7 @@ int main(int argc, char** argv)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WINDOW_W, WINDOW_H, 0, GL_RGB, GL_FLOAT, NULL);
 
 	// Init tile texture.
-	g_TileTexture = fsLoadTexture2D("com.rhythm.randomTile128.jpg", &g_TileW, &g_TileH);
+	g_TileTexture = fsLoadTexture2D("textures/crystal-spores.jpg", &g_TileW, &g_TileH);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
